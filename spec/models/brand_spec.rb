366a654819ceccef5895d810ae4cf915b9d7d5f2
Vlad Matiyko <vmatiyko@gmail.com>
2026-05-31 18:38:31 +0400

    Validate brand name uniqueness case-insensitively

--- spec/models/brand_spec.rb
+++ spec/models/brand_spec.rb
@@ -7,5 +7,14 @@ RSpec.describe Brand, type: :model do
 
   describe "validations" do
     it { is_expected.to validate_presence_of(:name) }
+
+    it "validates name uniqueness case-insensitively before the database constraint" do
+      create(:brand, name: "Apple")
+
+      duplicate_brand = build(:brand, name: "apple")
+
+      expect(duplicate_brand).not_to be_valid
+      expect(duplicate_brand.errors[:name]).to include("has already been taken")
+    end
   end
 end
